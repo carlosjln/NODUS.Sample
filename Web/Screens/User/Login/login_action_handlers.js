98ae3ccd1c305472547f419e63c02d1e2194d9bd
carlosjln <carlosjln01@gmail.com>
2013-10-15 07:30:32 -0400

    minor changes

--- Web/Screens/User/Login/login_action_handlers.js
+++ Web/Screens/User/Login/login_action_handlers.js
@@ -10,46 +10,12 @@ var request_status;
 // var alerts;
 var alert;
 
-var Form = {
-	enhance: function( form ) {
-		form.find("input").on( "focus", function( evt ) {
-			// console.log( $(this).parent(".input") );
-		} );
-		
-//		$('[placeholder]').focus(function() {
-//			var input = $(this);
-//			
-//			if (input.val() == input.attr('placeholder')) {
-//				input.val('');
-//				input.removeClass('placeholder');
-//			}
-//		}).blur( function() {
-//			var input = $(this);
-//			
-//			if (input.val() == '' || input.val() == input.attr('placeholder')) {
-//				input.addClass('placeholder');
-//				input.val( input.attr('placeholder') );
-//			}
-//		}).blur();
-//		
-//		$('[placeholder]').parents('form').submit(function() {
-//			$(this).find('[placeholder]').each(function() {
-//				var input = $(this);
-//				if (input.val() == input.attr('placeholder')) input.val('');
-//			});
-//		});
-	}
-};
-
-window.Form = Form;
-
 Handle.templates = {
 	main : {
 		on_success: function( template ) {
 			$('body').html( template.html );
 			
 			var login = $('#login');
-			Form.enhance(login);
 			
 			login_button = login.find("button[type=submit]");
 			
